28d81f1d094f9cd8aa3a73bd66e6cf2c59649055
Nowshin Fabiha Ibnat <ibnatnstu@gmail.com>
2026-02-17 08:19:45 +0600

    add configuraation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-import 'package:brick_braker_game/src/brick_breaker.dart';
-import 'package:flame/game.dart';
+import 'package:brick_braker_game/widgets/game_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  final game = BrickBreaker();                                  // Modify this line
-  runApp(GameWidget(game: game));
+  runApp(const GameApp());
 }
